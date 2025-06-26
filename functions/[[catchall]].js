3cd8e16774272832d47e0c23fd2d34f23f1e3e1c
allwin88 <clarinegeisinskyhtl83@gmail.com>
2025-06-26 20:26:08 +0800

    Delete functions directory

--- functions/[[catchall]].js
+++
@@ -1,7 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url)
-  if (!url.pathname.endsWith(".html") && !url.pathname.includes(".")) {
-    return fetch(new URL("/index.html", context.request.url))
-  }
-  return context.next()
-}
--- functions/catchall.js
+++
@@ -1,3 +0,0 @@
-export async function onRequest(context) {
-  return new Response(await context.env.ASSETS.fetch(context.request));
-}
