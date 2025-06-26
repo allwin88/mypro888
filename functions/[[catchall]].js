9a96862879304f807fb2006d0d422a06520c1dc4
allwin88 <clarinegeisinskyhtl83@gmail.com>
2025-06-26 20:07:49 +0800

    Add files via upload

--- functions/[[catchall]].js
+++ functions/[[catchall]].js
@@ -1,16 +1,15 @@
 
 export async function onRequest(context) {
   const url = new URL(context.request.url)
-  const path = url.pathname === "/" ? "/index.html" : url.pathname
+  const pathname = url.pathname
 
-  if (path.endsWith(".html")) {
-    let html = await context.env.ASSETS.fetch("https://stationen.pro" + path)
-    let content = await html.text()
-    content = content.replace(/theboss\.casino/gi, "stationen.pro")
-                     .replace(/TheBoss\.Casino/gi, "Stationen")
-                     .replace(/<title>(.*?)<\/title>/i, "<title>Stationen Sweepstakes</title>")
-    return new Response(content, { headers: { "content-type": "text/html;charset=UTF-8" } })
+  if (pathname === '/' || pathname === '/index.html') {
+    return context.next()
   }
 
-  return fetch("https://stationen.pro" + path)
+  return fetch(`https://theboss.casino${pathname}`, {
+    headers: context.request.headers,
+    method: context.request.method,
+    body: context.request.body
+  })
 }
